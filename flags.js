90a872ee6da96147ae45270c3421b36848b267a5
Evans nii <evansniiodarteyb@gmail.com>
2025-04-10 13:16:11 -0700

    i just added an html css and js file

---
+++ flags.js
@@ -0,0 +1,273 @@
+const flags = [
+    {
+      "name": "Afghanistan",
+      "flag": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"
+    },
+    {
+      "name": "Åland Islands",
+      "flag": "https://flagcdn.com/ax.svg"
+    },
+    {
+      "name": "Albania",
+      "flag": "https://flagcdn.com/al.svg"
+    },
+    {
+      "name": "Algeria",
+      "flag": "https://flagcdn.com/dz.svg"
+    },
+    {
+      "name": "American Samoa",
+      "flag": "https://flagcdn.com/as.svg"
+    },
+    {
+      "name": "Andorra",
+      "flag": "https://flagcdn.com/ad.svg"
+    },
+    {
+      "name": "Angola",
+      "flag": "https://flagcdn.com/ao.svg"
+    },
+    {
+      "name": "Anguilla",
+      "flag": "https://flagcdn.com/ai.svg"
+    },
+    {
+      "name": "Antarctica",
+      "flag": "https://flagcdn.com/aq.svg"
+    },
+    {
+      "name": "Antigua and Barbuda",
+      "flag": "https://flagcdn.com/ag.svg"
+    },
+    {
+      "name": "Argentina",
+      "flag": "https://flagcdn.com/ar.svg"
+    },
+    {
+      "name": "Armenia",
+      "flag": "https://flagcdn.com/am.svg"
+    },
+    {
+      "name": "Aruba",
+      "flag": "https://flagcdn.com/aw.svg"
+    },
+    {
+      "name": "Australia",
+      "flag": "https://flagcdn.com/au.svg"
+    },
+    {
+      "name": "Austria",
+      "flag": "https://flagcdn.com/at.svg"
+    },
+    {
+      "name": "Azerbaijan",
+      "flag": "https://flagcdn.com/az.svg"
+    },
+    {
+      "name": "Bahamas",
+      "flag": "https://flagcdn.com/bs.svg"
+    },
+    {
+      "name": "Bahrain",
+      "flag": "https://flagcdn.com/bh.svg"
+    },
+    {
+      "name": "Bangladesh",
+      "flag": "https://flagcdn.com/bd.svg"
+    },
+    {
+      "name": "Barbados",
+      "flag": "https://flagcdn.com/bb.svg"
+    },
+    {
+      "name": "Belarus",
+      "flag": "https://flagcdn.com/by.svg"
+    },
+    {
+      "name": "Belgium",
+      "flag": "https://flagcdn.com/be.svg"
+    },
+    {
+      "name": "Belize",
+      "flag": "https://flagcdn.com/bz.svg"
+    },
+    {
+      "name": "Benin",
+      "flag": "https://flagcdn.com/bj.svg"
+    },
+    {
+      "name": "Bermuda",
+      "flag": "https://flagcdn.com/bm.svg"
+    },
+    {
+      "name": "Bhutan",
+      "flag": "https://flagcdn.com/bt.svg"
+    },
+    {
+      "name": "Bolivia (Plurinational State of)",
+      "flag": "https://flagcdn.com/bo.svg"
+    },
+    {
+      "name": "Bonaire, Sint Eustatius and Saba",
+      "flag": "https://flagcdn.com/bq.svg"
+    },
+    {
+      "name": "Bosnia and Herzegovina",
+      "flag": "https://flagcdn.com/ba.svg"
+    },
+    {
+      "name": "Botswana",
+      "flag": "https://flagcdn.com/bw.svg"
+    },
+    {
+      "name": "Bouvet Island",
+      "flag": "https://flagcdn.com/bv.svg"
+    },
+    {
+      "name": "Brazil",
+      "flag": "https://flagcdn.com/br.svg"
+    },
+    {
+      "name": "British Indian Ocean Territory",
+      "flag": "https://flagcdn.com/io.svg"
+    },
+    {
+      "name": "United States Minor Outlying Islands",
+      "flag": "https://flagcdn.com/um.svg"
+    },
+    {
+      "name": "Virgin Islands (British)",
+      "flag": "https://flagcdn.com/vg.svg"
+    },
+    {
+      "name": "Virgin Islands (U.S.)",
+      "flag": "https://flagcdn.com/vi.svg"
+    },
+    {
+      "name": "Brunei Darussalam",
+      "flag": "https://flagcdn.com/bn.svg"
+    },
+    {
+      "name": "Bulgaria",
+      "flag": "https://flagcdn.com/bg.svg"
+    },
+    {
+      "name": "Burkina Faso",
+      "flag": "https://flagcdn.com/bf.svg"
+    },
+    {
+      "name": "Burundi",
+      "flag": "https://flagcdn.com/bi.svg"
+    },
+    {
+      "name": "Cambodia",
+      "flag": "https://flagcdn.com/kh.svg"
+    },
+    {
+      "name": "Cameroon",
+      "flag": "https://flagcdn.com/cm.svg"
+    },
+    {
+      "name": "Canada",
+      "flag": "https://flagcdn.com/ca.svg"
+    },
+    {
+      "name": "Cabo Verde",
+      "flag": "https://flagcdn.com/cv.svg"
+    },
+    {
+      "name": "Cayman Islands",
+      "flag": "https://flagcdn.com/ky.svg"
+    },
+    {
+      "name": "Central African Republic",
+      "flag": "https://flagcdn.com/cf.svg"
+    },
+    {
+      "name": "Chad",
+      "flag": "https://flagcdn.com/td.svg"
+    },
+    {
+      "name": "Chile",
+      "flag": "https://flagcdn.com/cl.svg"
+    },
+    {
+      "name": "China",
+      "flag": "https://flagcdn.com/cn.svg"
+    },
+    {
+      "name": "Christmas Island",
+      "flag": "https://flagcdn.com/cx.svg"
+    },
+    {
+      "name": "Cocos (Keeling) Islands",
+      "flag": "https://flagcdn.com/cc.svg"
+    },
+    {
+      "name": "Colombia",
+      "flag": "https://flagcdn.com/co.svg"
+    },
+    {
+      "name": "Comoros",
+      "flag": "https://flagcdn.com/km.svg"
+    },
+    {
+      "name": "Congo",
+      "flag": "https://flagcdn.com/cg.svg"
+    },
+    {
+      "name": "Congo (Democratic Republic of the)",
+      "flag": "https://flagcdn.com/cd.svg"
+    },
+    {
+      "name": "Cook Islands",
+      "flag": "https://flagcdn.com/ck.svg"
+    },
+    {
+      "name": "Costa Rica",
+      "flag": "https://flagcdn.com/cr.svg"
+    },
+    {
+      "name": "Croatia",
+      "flag": "https://flagcdn.com/hr.svg"
+    },
+    {
+      "name": "Cuba",
+      "flag": "https://flagcdn.com/cu.svg"
+    },
+    {
+      "name": "Curaçao",
+      "flag": "https://flagcdn.com/cw.svg"
+    },
+    {
+      "name": "Cyprus",
+      "flag": "https://flagcdn.com/cy.svg"
+    },
+    {
+      "name": "Czech Republic",
+      "flag": "https://flagcdn.com/cz.svg"
+    },
+  ]
+let containerid =document.getElementById("containerid")
+console.log(containerid)  
+{/* <div class="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg" alt="Flag-of-Afghanistan">
+            <h3>AFGHANISTAN</h3>
+        </div> */}
+
+function displayedCountries(countries) {
+    containerid.innerHTML =""
+    countries.forEach(element => {
+        const card =document.createElement("div")
+        card.classList.add("card")
+        const flag =document.createElement("img")
+        flag.src =element.flag
+        flag.alt =`${element.name} flag`
+        const countryName =document.createElement("h3")
+        countryName.innerHTML =element.name
+        card.append(flag)
+        card.append(countryName)
+        containerid.append(card)
+    });
+}
+displayedCountries(flags)
